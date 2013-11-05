d51e010109844161c9629a4ba2f86b487f7baa76
Bryan Heidorn <pbryan.heidorn@gmail.com>
2013-11-05 15:45:15 -0700

    Adjusting problems with blank and null lines

---
+++ herb/gold/parsed/fix.sh
@@ -0,0 +1,14 @@
+vi NY_01497989.csv
+vi NY_01497992.csv
+vi NY_01497997.csv
+vi NY_01498009.csv
+vi NY_01498031.csv
+vi NY_01498039.csv
+vi NY_01498043.csv
+vi NY_01498062.csv
+vi NY_01498065.csv
+vi NY_01498067.csv
+vi NY_01498069.csv
+vi NY_01498080.csv
+vi NY_01498080b.csv
+vi NY_01498087.csv
